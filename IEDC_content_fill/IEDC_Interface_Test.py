346a5e015a66f67fa34bd82481ea157d7a2d4d1b
= <=>
2018-07-31 10:44:48 +0200

    Added TOC upload script, and database testing script.

---
+++ IEDC_content_fill/IEDC_Interface_Test.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 14:01:15 2018
+
+@author: spauliuk
+"""
+
+import pymysql
+import datetime
+import IEDC_PW
+
+
+conn = pymysql.connect(host='www.industrialecology.uni-freiburg.de', port=3306, user=IEDC_PW.IEDC_write_access_user, passwd=IEDC_PW.IEDC_write_access_user_PW, db='iedc_review', autocommit=True, charset='utf8')
+#conn = pymysql.connect(host='www.industrialecology.uni-freiburg.de', port=3306, user=IEDC_PW.IEDC_write_access_user, passwd=IEDC_PW.IEDC_write_access_user_PW, db='iedc', autocommit=True, charset='utf8')
+
+cur = conn.cursor()
+
+cur.execute("Show tables")
+for row in cur:
+    print(row)
+
+cur.execute("DESCRIBE iedc_review.datasets")
+DSD = []
+for row in cur:
+    print(row)
+    DSD.append(row)
+
+# Select all from table:
+cur.execute("DESCRIBE iedc_review.licences")
+for row in cur:
+    print(row)
+
+cur.execute("SELECT * FROM licences")
+for row in cur:
+    print(row)
+    
+cur.execute("SELECT * FROM iedc_review.datasets")
+for row in cur:
+    print(row)
+    
+cur.execute("SELECT * FROM datasets")
+for row in cur:
+    print(row)
+
+    
+# Show table creation statement    
+cur.execute("SHOW CREATE TABLE licences")
+for row in cur:
+    print(row)
+    
+cur.execute("SHOW CREATE TABLE datasets")
+for row in cur:
+    print(row)    
+    
+cur.execute("SHOW TABLE STATUS FROM iedc_review WHERE `name` LIKE 'datasets'")    
+for row in cur:
+    print(row)
+    
+# Get current auto_increment values:    
+cur.execute("SELECT table_name, Auto_increment FROM information_schema.tables WHERE table_schema = DATABASE()")    
+for row in cur:
+    print(row)
+    
+# Change auto_increment    
+cur.execute("ALTER TABLE iedc_review.licences AUTO_INCREMENT = 5")
+cur.execute("ALTER TABLE iedc_review.datasets AUTO_INCREMENT = 1")
+    
+# Insert data into tables with auto_increment and default NULL columns:
+SQL = "INSERT INTO iedc_review.licences (name,description) VALUES ('Crown Copyright','')"
+cur.execute(SQL)
+D = ['Test','']
+SQL = "INSERT INTO iedc_review.licences (name,description) VALUES (%s,%s)"
+cur.execute(SQL,('Test',''))
+    
+# Delete
+cur.execute("DELETE FROM iedc_review.licences WHERE id = 5")    
+cur.execute("DELETE FROM iedc_review.datasets WHERE id = 1") 
+
+cur.execute("DELETE FROM iedc_review.datasets")
+
+# Insert into datasets:
+# id is not set because of auto_increment
+
+
+
+
+
+
+conn.commit()
+
+
+
+
+cur.execute("SELECT * FROM datasets WHERE id = 58")
+for row in cur:
+    print(row)
+
+#
+#    
+#    
+# The End
