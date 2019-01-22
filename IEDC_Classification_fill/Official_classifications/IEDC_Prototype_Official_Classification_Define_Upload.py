80f624fb00098cc9f5ff544183750f08ad7866c9
= <stefanpauliuk@gmx.de>
2019-01-22 23:21:23 +0100

    Added upload script for new classifications from xlsx template.

--- IEDC_Classification_fill/Official_classifications/IEDC_Prototype_Official_Classification_Define_Upload.py
+++ IEDC_Classification_fill/Official_classifications/IEDC_Prototype_Official_Classification_Define_Upload.py
@@ -21,8 +21,42 @@ def from_excel_ordinal(ordinal, _epoch=date(1900, 1, 1)):
         ordinal -= 1  # Excel leap year bug, 1900 is not a leap year!
     return _epoch + timedelta(days=ordinal - 1)  # epoch is day 1
 
-# Define mySQL command for dataset insertion
-SQL = "INSERT INTO classification_items (\
+
+ClassList   = ['EXIOBASEv3_163Industries','EXIOBASEv3_163Products','EXIOBASEv3_200Products'] # List of filenames for classifications to be added.
+ClassIDList = [25,26,27]
+
+# Define mySQL commands for classification
+SQLD = "INSERT INTO classification_definition (\
+id,\
+classification_name,\
+dimension,\
+description,\
+mutually_exclusive,\
+collectively_exhaustive,\
+general,\
+created_from_dataset,\
+reference,\
+reserve1,\
+reserve2,\
+reserve3,\
+meaning_attribute1,\
+meaning_attribute2,\
+meaning_attribute3,\
+meaning_attribute4,\
+meaning_attribute5,\
+meaning_attribute6,\
+meaning_attribute7,\
+meaning_attribute8,\
+meaning_attribute9,\
+meaning_attribute10,\
+meaning_attribute11,\
+meaning_attribute12,\
+meaning_attribute13,\
+meaning_attribute14,\
+meaning_attribute15) Values(\
+%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+SQLI = "INSERT INTO classification_items (\
 classification_id,\
 parent_id,\
 description,\
@@ -44,72 +78,79 @@ attribute14_anc,\
 attribute15_anc) Values(\
 %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
-#conn = pymysql.connect(host='www.industrialecology.uni-freiburg.de', port=3306, user=IEDC_PW.IEDC_write_access_user, passwd=IEDC_PW.IEDC_write_access_user_PW, db='iedc_review', autocommit=True, charset='utf8')
-conn = pymysql.connect(host='www.industrialecology.uni-freiburg.de', port=3306, user=IEDC_PW.IEDC_write_access_user, passwd=IEDC_PW.IEDC_write_access_user_PW, db='iedc', autocommit=True, charset='utf8')
+conn = pymysql.connect(host='www.industrialecology.uni-freiburg.de', port=3306, user=IEDC_PW.IEDC_write_access_user, passwd=IEDC_PW.IEDC_write_access_user_PW, db='iedc_review', autocommit=True, charset='utf8')
+#conn = pymysql.connect(host='www.industrialecology.uni-freiburg.de', port=3306, user=IEDC_PW.IEDC_write_access_user, passwd=IEDC_PW.IEDC_write_access_user_PW, db='iedc', autocommit=True, charset='utf8')
 
 cur = conn.cursor()
 
-# Scan folder for .csv files
-f = []
-for (dirpath, dirnames, filenames) in walk(IEDC_Paths.Class_ItemPath):
-    f.extend(filenames)
-    break
+for m in range(0,len(ClassList)):
+    ClassFilePath = path.join(path.join(IEDC_Paths.Class_ItemPath,'Official_classifications'),ClassList[m] + '.xlsx')
+    ClassFile = xlrd.open_workbook(ClassFilePath)
+    ClassDefsheet = ClassFile.sheet_by_name('Definition')
 
-for filename in f:
-    FilePath = path.join(IEDC_Paths.Class_ItemPath,filename)
-    print(FilePath)
-    reservedcount = 1
-    DInsert = False
-    with open(FilePath, encoding = 'utf-8') as fi:
-        reader = csv.reader(fi, delimiter=';', quotechar='"')
-        for cols in reader:
-            # parse row with items and insert into mysql
-            try:  # Check whether line contains classification item
-                Id = int(cols[1])
-            except:
-                Id = -1
-            if Id != -1:
-                D = [] # Data items list
-                D.append(Id)
-                for m in range(2,20):
-                    if cols[m] == '':
-                        D.append(None)
-                    elif cols[m] == 'reserved':
-                        D.append('reserved_' + str(reservedcount))
-                        reservedcount +=1
-                    else:
-                        D.append(cols[m])
-                # Add data into db:
-                cur.execute(SQL,(D[0],D[1],D[2],D[3],D[4],D[5],D[6],D[7],D[8],D[9],D[10],\
-                         D[11],D[12],D[13],D[14],D[15],D[16],D[17],D[18]))
-                print(D[4])
-                
+    C_id       = int(ClassDefsheet.cell_value(1,1))
+    C_name     = ClassDefsheet.cell_value(2,1)
+    C_Dim      = int(ClassDefsheet.cell_value(3,1))
+    OtherItems = [] 
+    for n in range(0,24):
+        if ClassDefsheet.cell_value(4+n,1) == 'True':
+            OtherItems.append(1)
+        elif ClassDefsheet.cell_value(4+n,1) == 'False':
+            OtherItems.append(0)                        
+        elif ClassDefsheet.cell_value(4+n,1) != '':
+            OtherItems.append(ClassDefsheet.cell_value(4+n,1))            
+        else:
+            OtherItems.append(None)
+
+    # Define classification
+    cur.execute(SQLD,(C_id,C_name,C_Dim,OtherItems[0],OtherItems[1],OtherItems[2],OtherItems[3],OtherItems[4],OtherItems[5],OtherItems[6],OtherItems[7],\
+                         OtherItems[8],OtherItems[9],OtherItems[10],OtherItems[11],OtherItems[12],OtherItems[13],OtherItems[14],OtherItems[15],\
+                         OtherItems[16],OtherItems[17],OtherItems[18],OtherItems[19],OtherItems[20],OtherItems[21],OtherItems[22],OtherItems[23]))
+
+    # Read items, take attribute1_oto as reference:
+    ClassItemsheet = ClassFile.sheet_by_name('Items')
+    n = 1
+    while True:
+        try:
+            ThisItem = ClassItemsheet.cell_value(n,5)
+        except:
+            break # break
+        Items = []
+        for o in range(0,18):
+            if ClassItemsheet.cell_value(n,o+2) != '':
+                Items.append(ClassItemsheet.cell_value(n,o+2))            
+            else:
+                Items.append(None)    
+        # insert classification item                
+        cur.execute(SQLI,(C_id,Items[0],Items[1],Items[2],Items[3],Items[4],Items[5],Items[6],Items[7],Items[8],Items[9],Items[10],\
+                         Items[11],Items[12],Items[13],Items[14],Items[15],Items[16],Items[17]))
+        n +=1
+        
+        
+        
+        
+        
+# Delete existing classifications:
+#cur.execute("DELETE FROM classification_items WHERE classification_id = 25") 
+#cur.execute("DELETE FROM classification_definition WHERE id = 25") 
+#cur.execute("SELECT * FROM classification_definition WHERE id = 25")
+#for row in cur:
+#    print(row)
+
             
 
 # Check
-cur.execute("SELECT COUNT(*) FROM classification_items")
+Value = 25
+cur.execute("SELECT COUNT(*) FROM classification_items WHERE classification_id =%s",(Value))
 for row in cur:
     print(row)    
 
-# Other
-#cur.execute("SELECT * FROM classification_items")
-#for row in cur:
-#    print(row)  
-#    
-#cur.execute("DELETE FROM classification_items")    
-#cur.execute("ALTER TABLE classification_items AUTO_INCREMENT = 1")
+cur.execute("SELECT * FROM classification_items WHERE classification_id =%s",(Value))
+for row in cur:
+    print(row)  
     
-#    FilePath = 'C:\\Users\\spauliuk\\FILES\\ARBEIT\\PROJECTS\\Database\\IE_DataCommons\\Software\\IEDC_Classification_fill\\generic_materials_waste_data.csv'    
-
-# Extra: Add leading 0 where missing for classification NACEv2 (id = 5)
-#cur.execute("SELECT COUNT(*) FROM classification_items WHERE classification_id = 5 AND LENGTH(attribute1_oto) = 7")
-#for row in cur:
-#    print(row)   
-#    
-#cur.execute("SELECT attribute1_oto FROM classification_items WHERE classification_id = 5 AND LENGTH(attribute1_oto) = 7")    
-#for row in cur:    
-#    print(row[0])
-#    cur.execute("UPDATE classification_items SET attribute1_oto = %s WHERE attribute1_oto = %s",('0' + row[0], row[0])) 
+    
+    
 
 # Close connection
 cur.close()
